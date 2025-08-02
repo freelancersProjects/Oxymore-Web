44e62acf6de780833bb5502ff043278be07df27c
MathisBls <mathis.oxym@gmail.com>
2025-08-03 00:21:19 +0200

    cors vercel patch

--- back/src/index.ts
+++ back/src/index.ts
@@ -12,10 +12,17 @@ const app = express();
 const server = createServer(app);
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "https://oxymore-web-oxymore-admin.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
